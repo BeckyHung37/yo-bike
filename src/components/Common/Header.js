c26cb7d3e394a42954773278474a8fbb06b811f2
mshmwr <mshmwr20@gmail.com>
2021-11-28 14:30:39 +0800

    modify: navbar spot link redirect to yo-taiwan page

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div className="header">
       <div className="header_container">
-        <Link to="/yoBike">{getYoBikeLogoIcon()}</Link>
+        <Link to="/">{getYoBikeLogoIcon()}</Link>
         <div className="nav_menu">
           <Link to="/" className="menu_rentbike">
             租/還車即時資訊
@@ -24,14 +24,19 @@ const Header = () => {
               alt=""
             />
           </Link>
-          <Link to="/" className="menu_landscape">
+          <a
+            href="https://mshmwr.github.io/yo-taiwan/"
+            target="_blank"
+            rel="noreferrer"
+            className="menu_landscape"
+          >
             觀光景點
             <img
               className="wave_yellow"
               src="./static/icon/wave_yellow.png"
               alt=""
             />
-          </Link>
+          </a>
           <button className="btn_callforaction">今天想去哪？</button>
         </div>
       </div>
