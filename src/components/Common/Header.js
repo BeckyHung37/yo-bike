05a5f6e3ddecfe70673517f4964262d5a90b2e91
BeckyHung37 <beckyhung37@gmail.com>
2021-11-28 18:23:46 +0800

    adjust nav menu and search bar

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -10,19 +10,9 @@ const Header = () => {
         <div className="nav_menu">
           <Link to="/" className="menu_rentbike">
             租/還車即時資訊
-            <img
-              className="wave_yellow_long"
-              src="./static/icon/wave_yellow_long.png"
-              alt=""
-            />
           </Link>
           <Link to="/" className="menu_routeinfo">
             路線資訊
-            <img
-              className="wave_yellow"
-              src="./static/icon/wave_yellow.png"
-              alt=""
-            />
           </Link>
           <a
             href="https://mshmwr.github.io/yo-taiwan/"
@@ -31,11 +21,6 @@ const Header = () => {
             className="menu_landscape"
           >
             觀光資訊
-            <img
-              className="wave_yellow"
-              src="./static/icon/wave_yellow.png"
-              alt=""
-            />
           </a>
           <button className="btn_callforaction">今天想去哪？</button>
         </div>
